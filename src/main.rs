0edd7419b9e217e4b15e67ac69bf827a395d6106
Pedro Alves <pta2002@pta2002.com>
2020-05-14 22:39:55 +0100

    fix build

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use aubio_rs::{Pitch, PitchMode};
-use jack::Client as JackClient;
 use jack::PortFlags as PF;
 use std::convert::TryInto;
-use std::sync::{Arc, Mutex};
 
 fn main() {
     // Initialize JACK
-    let (client, _) = JackClient::new("tuner", jack::ClientOptions::NO_START_SERVER).unwrap();
+    let (client, _) = jack::Client::new("tuner", jack::ClientOptions::NO_START_SERVER).unwrap();
 
     let input_port = client
         .register_port("input", jack::AudioIn)
@@ -14,28 +12,28 @@ fn main() {
 
     let input_name = input_port.name().unwrap();
 
-    let mut pitch_detector = Arc::new(Mutex::new(
-        Pitch::new(
-            PitchMode::Yinfft,
-            16384,
-            client.buffer_size().try_into().unwrap(),
-            client.sample_rate().try_into().unwrap(),
-        )
-        .expect("Failed to initialize pitch detector"),
-    ));
+    let mut pitch_detector = Pitch::new(
+        PitchMode::Yinfft,
+        16384,
+        client.buffer_size().try_into().unwrap(),
+        client.sample_rate().try_into().unwrap(),
+    )
+    .expect("Failed to initialize pitch detector");
 
-    let process_callback = |client: &jack::Client, ps: &jack::ProcessScope| {
-        println!("Got {} frames!", ps.n_frames());
+    let (tx, rx) = crossbeam_channel::unbounded();
 
-        let audio_input = input_port.as_slice(ps);
-        let mut detector = pitch_detector.clone().lock().unwrap();
+    let process_callback = move |_: &jack::Client, ps: &jack::ProcessScope| {
+        let audio_input = input_port.as_slice(ps).to_owned();
+        tx.send(audio_input).unwrap();
 
-        println!("Freq: {}", detector.do_result(audio_input).unwrap_or(0.0));
+        // let mut detector = pitch_detector.clone().lock().unwrap();
+
+        // println!("Freq: {}", detector.do_result(audio_input).unwrap_or(0.0));
 
         jack::Control::Continue
     };
 
-    let process = jack::ClosureProcessHandler::new(process_callback, pitch_detector.clone());
+    let process = jack::ClosureProcessHandler::new(process_callback);
     let async_client = client.activate_async(Notifications, process).unwrap();
     let client = async_client.as_client();
 
@@ -45,7 +43,12 @@ fn main() {
         .connect_ports_by_name(&output_port, &input_name)
         .unwrap();
 
-    loop {}
+    while let Ok(audio_input) = rx.recv() {
+        println!(
+            "Freq: {}",
+            pitch_detector.do_result(audio_input).unwrap_or(0.0)
+        );
+    }
 }
 
 struct Notifications;
